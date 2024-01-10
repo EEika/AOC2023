4c9d8443ebb2fe7fc89ccc605cf970f86602048e
Espen Eika <espen.h.eika@gmail.com>
2024-01-10 10:56:30 +0100

    Add puzzle input reading and test case for group 1-2-3

--- day_12/src/main.rs
+++ day_12/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let p_i = &std::fs::read_to_string("puzzle_input.txt").unwrap()[..];
+
+    println!("{p_i}");
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn group_1_2_3() {
+        let solu = 0;
+        assert_eq!(solu, 1);
+    }
 }
